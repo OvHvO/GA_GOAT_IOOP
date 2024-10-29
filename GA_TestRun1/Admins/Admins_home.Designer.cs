28265f5bcef6dbbe1102e9ba0a99b18423da8436
PUA WAI KIT <TP081560@mail.apu.edu.my>
2024-10-29 19:08:34 +0800

    like

--- GA_TestRun1/Admins/Admins_home.Designer.cs
+++ GA_TestRun1/Admins/Admins_home.Designer.cs
@@ -32,11 +32,12 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.FeatureBox = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.service_MNG = new System.Windows.Forms.Button();
             this.staff_MNG = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.service_MNG = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.FeatureBox.SuspendLayout();
@@ -45,9 +46,10 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Monotype Corsiva", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(392, 13);
+            this.textBox1.Location = new System.Drawing.Point(261, 8);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(385, 52);
+            this.textBox1.Size = new System.Drawing.Size(258, 37);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "ADMIN INTERFACE";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -56,21 +58,24 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(-3, -2);
+            this.tabControl1.Location = new System.Drawing.Point(-2, -1);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(803, 456);
+            this.tabControl1.Size = new System.Drawing.Size(535, 296);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
             this.tabPage1.BackColor = System.Drawing.Color.PeachPuff;
+            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.FeatureBox);
             this.tabPage1.Controls.Add(this.textBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(795, 423);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(527, 270);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             // 
@@ -82,64 +87,81 @@
             this.FeatureBox.Controls.Add(this.service_MNG);
             this.FeatureBox.Controls.Add(this.staff_MNG);
             this.FeatureBox.Location = new System.Drawing.Point(0, 0);
+            this.FeatureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.FeatureBox.Name = "FeatureBox";
-            this.FeatureBox.Size = new System.Drawing.Size(124, 423);
+            this.FeatureBox.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FeatureBox.Size = new System.Drawing.Size(83, 275);
             this.FeatureBox.TabIndex = 1;
             this.FeatureBox.TabStop = false;
             this.FeatureBox.Text = "Features Box";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 218);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 21);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 160);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 21);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // service_MNG
+            // 
+            this.service_MNG.Location = new System.Drawing.Point(13, 96);
+            this.service_MNG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.service_MNG.Name = "service_MNG";
+            this.service_MNG.Size = new System.Drawing.Size(56, 21);
+            this.service_MNG.TabIndex = 1;
+            this.service_MNG.Text = "Service";
+            this.service_MNG.UseVisualStyleBackColor = true;
+            // 
             // staff_MNG
             // 
-            this.staff_MNG.Location = new System.Drawing.Point(20, 60);
+            this.staff_MNG.Location = new System.Drawing.Point(13, 39);
+            this.staff_MNG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.staff_MNG.Name = "staff_MNG";
-            this.staff_MNG.Size = new System.Drawing.Size(84, 33);
+            this.staff_MNG.Size = new System.Drawing.Size(56, 21);
             this.staff_MNG.TabIndex = 0;
             this.staff_MNG.Text = "Staff";
             this.staff_MNG.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(795, 423);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(527, 270);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // service_MNG
+            // button1
             // 
-            this.service_MNG.Location = new System.Drawing.Point(20, 148);
-            this.service_MNG.Name = "service_MNG";
-            this.service_MNG.Size = new System.Drawing.Size(84, 33);
-            this.service_MNG.TabIndex = 1;
-            this.service_MNG.Text = "Service";
-            this.service_MNG.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(20, 246);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(84, 33);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(20, 335);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(84, 33);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(123, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Admins_home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Admins_home";
             this.Text = "Admins_home";
             this.tabControl1.ResumeLayout(false);
@@ -161,5 +183,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button service_MNG;
+        private System.Windows.Forms.Button button1;
     }
 }
