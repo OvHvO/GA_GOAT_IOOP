04d46a21230c405ff7d92785207686d4ff151293
OvHvO <waikitpua1128@gmail.com>
2024-10-29 08:01:52 +0800

    create admin source

---
+++ GA_TestRun1/Admins/Admins_Source.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_TestRun1.Admins
+{
+    internal class Admins_Source
+    {
+
+    }
+}
