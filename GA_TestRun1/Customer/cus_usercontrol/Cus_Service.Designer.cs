8b3f82c9ed8e380d91ff518fb067d48651ad15d9
PUA WAI KIT <TP081560@mail.apu.edu.my>
2024-11-10 21:05:57 +0800

    loo

--- GA_TestRun1/Customer/cus_usercontrol/Cus_Service.Designer.cs
+++ GA_TestRun1/Customer/cus_usercontrol/Cus_Service.Designer.cs
@@ -51,7 +51,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(196, 30);
+            this.label1.Location = new System.Drawing.Point(183, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(162, 37);
             this.label1.TabIndex = 0;
