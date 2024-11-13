cf11cd75f532b6ee77ea4eb1a0da8428f7e328ca
NixonSee <nixonsee2006@gmail.com>
2024-11-13 10:22:59 +0800

    view

--- GA_TestRun1/Mechanics/Mecha_Option/Mechanic_View.cs
+++ GA_TestRun1/Mechanics/Mecha_Option/Mechanic_View.cs
@@ -1,4 +1,5 @@
-﻿using GA_TestRun1.Receptionist;
+﻿using Azure.Identity;
+using GA_TestRun1.Receptionist;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,7 +25,7 @@ namespace GA_TestRun1.Mechanics
         private void Mechanic_View_Load(object sender, EventArgs e)
         {
             this.BackColor = ColorTranslator.FromHtml("#EEEBE3");
-
+            LoadGridView();
         }
 
         private void rcp_appoimentBar_Paint(object sender, PaintEventArgs e)
@@ -44,11 +45,41 @@ namespace GA_TestRun1.Mechanics
 
         private void Mcn_GridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            
+        }
+
+        private void LoadGridView()
+        {
+        string userName = "OriUserName";
+        string newName = "OriNewUserName";
+
+        Mechanic mechanic = new Mechanic(Name);
+
+        var data = mechanic.ViewProfList(userName, newName);
+
+            if (data != null)
+            {
+                Mcn_GridView.DataSource = data;  // Show the DataTable to the GridView
+
+                // Set the selected index of Search_list, ensuring it has items
+                if (Search_list.Items.Count > 0)
+                {
+                    Mcn_GridView.AutoGenerateColumns = true;
+                    Search_list.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Failed to retrieve profile list. Please check for errors.");
+            }
+        }
+
+        private void reload_pict_Click(object sender, EventArgs e)
+        {
             Mechanic mechanic = new Mechanic(Name);
             string userName = "SomeUser";  // Replace with actual UserName value
             string newName = "SomeNewUser";
             Mcn_GridView.DataSource = mechanic.ViewProfList(userName, newName);
-            Search_list.SelectedIndex = 0;
         }
     }
 }
