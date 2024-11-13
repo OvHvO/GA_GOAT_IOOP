701db0f6fda913b2556a78aeaab32dea269136b9
OvHvO <waikitpua1128@gmail.com>
2024-11-13 12:00:58 +0800

    lol

--- GA_TestRun1/Admins/usercontrol/Admin_salesreport.Designer.cs
+++ GA_TestRun1/Admins/usercontrol/Admin_salesreport.Designer.cs
@@ -34,6 +34,8 @@
             this.SR_TotalExpenses_LB = new System.Windows.Forms.ListBox();
             this.SR_NetProfit_TB = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.SR_totalGrossProfit_TB = new System.Windows.Forms.TextBox();
+            this.SR_TotalExpenses_TB = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // SP_label
@@ -62,9 +64,9 @@
             "October",
             "November",
             "December"});
-            this.SR_Month_CB.Location = new System.Drawing.Point(248, 180);
+            this.SR_Month_CB.Location = new System.Drawing.Point(248, 156);
             this.SR_Month_CB.Name = "SR_Month_CB";
-            this.SR_Month_CB.Size = new System.Drawing.Size(307, 28);
+            this.SR_Month_CB.Size = new System.Drawing.Size(316, 28);
             this.SR_Month_CB.TabIndex = 1;
             this.SR_Month_CB.Text = "Month";
             this.SR_Month_CB.SelectedIndexChanged += new System.EventHandler(this.SR_Month_CB_SelectedIndexChanged);
@@ -73,41 +75,60 @@
             // 
             this.SR_GrossProfit_LB.FormattingEnabled = true;
             this.SR_GrossProfit_LB.ItemHeight = 20;
-            this.SR_GrossProfit_LB.Location = new System.Drawing.Point(248, 204);
+            this.SR_GrossProfit_LB.Location = new System.Drawing.Point(248, 181);
             this.SR_GrossProfit_LB.Name = "SR_GrossProfit_LB";
-            this.SR_GrossProfit_LB.Size = new System.Drawing.Size(307, 144);
+            this.SR_GrossProfit_LB.Size = new System.Drawing.Size(316, 144);
             this.SR_GrossProfit_LB.TabIndex = 2;
             // 
             // SR_TotalExpenses_LB
             // 
             this.SR_TotalExpenses_LB.FormattingEnabled = true;
             this.SR_TotalExpenses_LB.ItemHeight = 20;
-            this.SR_TotalExpenses_LB.Location = new System.Drawing.Point(248, 345);
+            this.SR_TotalExpenses_LB.Location = new System.Drawing.Point(248, 363);
             this.SR_TotalExpenses_LB.Name = "SR_TotalExpenses_LB";
-            this.SR_TotalExpenses_LB.Size = new System.Drawing.Size(307, 144);
+            this.SR_TotalExpenses_LB.Size = new System.Drawing.Size(316, 144);
             this.SR_TotalExpenses_LB.TabIndex = 3;
             // 
             // SR_NetProfit_TB
             // 
-            this.SR_NetProfit_TB.Location = new System.Drawing.Point(248, 486);
+            this.SR_NetProfit_TB.Location = new System.Drawing.Point(248, 545);
             this.SR_NetProfit_TB.Name = "SR_NetProfit_TB";
-            this.SR_NetProfit_TB.Size = new System.Drawing.Size(307, 26);
+            this.SR_NetProfit_TB.ReadOnly = true;
+            this.SR_NetProfit_TB.Size = new System.Drawing.Size(316, 26);
             this.SR_NetProfit_TB.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(348, 563);
+            this.button1.Location = new System.Drawing.Point(349, 596);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 44);
             this.button1.TabIndex = 5;
             this.button1.Text = "!!!NOTICE!!!";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // SR_totalGrossProfit_TB
+            // 
+            this.SR_totalGrossProfit_TB.Location = new System.Drawing.Point(248, 331);
+            this.SR_totalGrossProfit_TB.Name = "SR_totalGrossProfit_TB";
+            this.SR_totalGrossProfit_TB.ReadOnly = true;
+            this.SR_totalGrossProfit_TB.Size = new System.Drawing.Size(316, 26);
+            this.SR_totalGrossProfit_TB.TabIndex = 7;
+            // 
+            // SR_TotalExpenses_TB
+            // 
+            this.SR_TotalExpenses_TB.Location = new System.Drawing.Point(248, 513);
+            this.SR_TotalExpenses_TB.Name = "SR_TotalExpenses_TB";
+            this.SR_TotalExpenses_TB.ReadOnly = true;
+            this.SR_TotalExpenses_TB.Size = new System.Drawing.Size(316, 26);
+            this.SR_TotalExpenses_TB.TabIndex = 8;
+            // 
             // Admin_salesreport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Salmon;
+            this.Controls.Add(this.SR_TotalExpenses_TB);
+            this.Controls.Add(this.SR_totalGrossProfit_TB);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.SR_NetProfit_TB);
             this.Controls.Add(this.SR_TotalExpenses_LB);
@@ -130,5 +151,7 @@
         private System.Windows.Forms.ListBox SR_TotalExpenses_LB;
         private System.Windows.Forms.TextBox SR_NetProfit_TB;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox SR_totalGrossProfit_TB;
+        private System.Windows.Forms.TextBox SR_TotalExpenses_TB;
     }
 }
