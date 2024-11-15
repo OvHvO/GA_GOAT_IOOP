4b464f6fdc9532e3276ab41793033f01abcec7d9
NixonSee <nixonsee2006@gmail.com>
2024-11-16 01:44:40 +0800

    aa

--- GA_TestRun1/Mechanics/Mecha_Option/Mechanic_Main.cs
+++ GA_TestRun1/Mechanics/Mecha_Option/Mechanic_Main.cs
@@ -51,7 +51,7 @@ namespace GA_TestRun1.Mechanics.Mecha_Option
         {
             string[] newprof = (string[])Mechanic.RefreshProfile(Names);
             Name_lbl.Text = newprof[0];
-            PNum_lbl.Text = newprof[1].ToString();
+            PNum_lbl.Text = newprof[1];
         }
 
         private void Welcome_txt_Click(object sender, EventArgs e)
